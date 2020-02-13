d47d3ced1a04d2312a9daa44d4df44c57d9d35a6
Jucelino Novais <jucelinodev@gmail.com>
2020-02-13 14:26:06 -0300

    implements product listing and product creation

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common'
+import { ProductService } from './product.service'
+import { Product } from './product.entity'
+import { CreateProductDto } from './dto/create-product.dto'
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Get()
+  async index(): Promise<Product[]> {
+    return this.productService.getAllProducts()
+  }
+
+  @Post()
+  async store(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productService.createProduct(createProductDto)
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Repository } from 'typeorm'
+import { Product } from './product.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { CreateProductDto } from './dto/create-product.dto'
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async getAllProducts(): Promise<Product[]> {
+    return await this.productRepository.find()
+  }
+
+  //getProductById(){}
+
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const product = this.productRepository.create(createProductDto)
+    return await this.productRepository.save(product)
+  }
+
+  //updateProductById(){}
+
+  //deleteProductById(){}
+}
